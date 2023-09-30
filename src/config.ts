2ecd2f7549c8fd081c0dd6bcc4e9ac565c4c3774
estevam5s <estevamsouzalaureth@gmail.com>
2023-09-30 18:26:19 -0300

    :boom: revertendo

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@
 
 export const SITE_TITLE = 'What I use - Estevam Souza'
 export const SITE_DESCRIPTION = 'A curated list of the tech I use'
-export const HOMEPAGE_URL = 'https://estevamwebsite.dev.vercel.app'
+export const HOMEPAGE_URL = 'https://estevamwebsite.vercel.app'
